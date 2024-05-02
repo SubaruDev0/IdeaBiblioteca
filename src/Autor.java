f191ae5a6b88d0e1f3692147d6839813faedfa66
Subaru <subaru0.dev@gmail.com>
2024-05-02 18:33:57 -0400

    idea gestión de una Biblioteca

---
+++ src/Autor.java
@@ -0,0 +1,9 @@
+public class Autor {
+    public String nombre;
+    public String apellido;
+
+    public Autor(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
---
+++ src/Libro.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Libro {
+    public String titulo;
+    public List<Autor> autores;
+
+    public Libro(String titulo, Autor... autores) {
+        this.titulo = titulo;
+        this.autores = new ArrayList<>();
+
+        this.autores.addAll(Arrays.asList(autores));
+    }
+
+        }
